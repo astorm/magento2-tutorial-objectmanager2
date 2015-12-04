720e7d7d213de64211d85d26b69b67cfbfb94003
Alan Storm <astorm@alanstorm.com>
2015-12-04 11:29:15 -0800

    Adding registration.php

---
+++ app/code/Pulsestorm/TutorialObjectManager2/registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Pulsestorm_TutorialObjectManager2',
+        __DIR__
+    );
